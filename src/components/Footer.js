cc354e057c845a1131ee7ee7af9f0a6a19b33390
manfredmontero <jmontero0590@icloud.com>
2023-02-13 18:17:57 -0600

    Adding styles and finishing the cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,40 @@
 import React from "react";
-import logolink from '../images/linkdln.png'
-import logoGit from '../images/github.jpg'
+import logolink from "../images/linkdln.png";
+import logoGit from "../images/github.png";
 
 function Footer() {
   return (
-    <div className="container mt-5">
-      <div className="text-center p-4 footer-div">
-        <div>© 2021 Copyright: Jorge Montero
-            <hr/>
-            <a href="https:www.linkedin.com/in/jorge--montero" target="_blank" >
-                <img src={logolink} className="logo-footer"/>
-            </a>
-            <a href="https:www.linkedin.com/in/jorge--montero" target="_blank" >
-                <img src={logoGit} className="logo-footer"/>
-            </a>
-
+    <div className="centerdiv text-center mt-5">
+      <div className=" p-4 footer-div">
+        <div className="footer-small-container">
+          © 2023 Copyright: Jorge Montero
+          <hr />
+          <a href="https:www.linkedin.com/in/jorge--montero" target="_blank">
+            <img
+              src="https://spng.pngfind.com/pngs/s/57-572039_12-linkedin-icon-flat-images-round-daily-dot.png"
+              className="logo-footer"
+            />
+          </a>
+          <a
+            href="https://github.com/manfredmontero"
+            target="_blank"
+            className=" ms-5"
+          >
+            <img
+              src="https://w7.pngwing.com/pngs/12/369/png-transparent-black-and-white-cat-illustration-github-computer-icons-icon-design-github-cat-like-mammal-carnivoran-logo.png"
+              className="logo-footer"
+            />
+          </a>
+          <a
+            href="mailto:jmontero0590@icloud.com?subject=I Came from your personal portafolio"
+            className=" ms-5"
+          >
+            <img
+              src="https://cdn4.vectorstock.com/i/1000x1000/03/68/envelope-icon-mail-symbol-black-silhouette-vector-20390368.jpg"
+              className="logo-footer"
+            />
+          </a>
         </div>
-
-
       </div>
       <div></div>
     </div>
--- src/components/Information.js
+++ src/components/Information.js
@@ -5,39 +5,52 @@ function Information() {
     <div className="container mt-5">
       <div className="row row-cols-1 row-cols-md-3 g-4">
         <div className="col">
-          <div className="card">
+          <div className="card roundedCards">
             <div className="card-body">
               <h4 className="card-title">ABOUT ME</h4>
+              <hr className="hr-line" />
               <p className="card-text">
                 + 8 years in Quality Assurance and programming. I have been part
-                of different teams and different cultures, I like team work. To
-                give ideas and hear everybody is one of my preferred way of
-                work. Everybody has something to say.
-                I love to workout and visit new places. Get to know new people and understand their ideas.
-                Always everybody's idea is important
+                of different teams and different cultures, I like the team work
+                and to give ideas and hear everybody is one of my preferred way
+                of work. Everybody has something to say.
+                <br />I love to workout and visit new places. Get to know new
+                people and understand their ideas. Always everybody's ideas are
+                important. For me, the comunication is the clue.
               </p>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card roundedCards">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h4 className="card-title">QUALITY ASSURANCE</h4>
+              <hr className="hr-line" />
               <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                As a QA Enginner, I have experience in Front End and Back End
+                testing, also I have experience in performance testing. <br />
+                I have the ISTQB certification and also I know how to create UI
+                Automated test ussing Cypress and Selenium.
+                <br />
+                For the Back End testing automation, I know to use javascript to
+                create API tests integrated with mocha and chai.
               </p>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="card">
+          <div className="card roundedCards">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h4 className="card-title">FULL STACK DEVELOPMENT</h4>
+              <hr className="hr-line" />
               <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content.
+                As a full stack student, I have learned how to create full stack
+                responisive projects from zero. I understand how does html and
+                css integrated work and also how can we integrate all the
+                resources. I know how do the http methods work and how to use
+                them to connetct to mongoose database.
+                <br />I am able to work with react and express, to create and
+                use the CRUD expresions between the Backend and the FrontEnd
               </p>
             </div>
           </div>
